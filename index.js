e40e962adcd3a14aed3f071020b23d877cab679c
Tsubasa Namiki <namikingsoft@gmail.com>
2018-06-29 06:55:01 +0900

    feat: hello world

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ module.exports = app => {
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
+  app.on('issues.opened', async context => {
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
+    const params = context.issue({body: 'Hello World!'})
+
+    // Post a comment on the issue
+    return context.github.issues.createComment(params)
+  })
 }
