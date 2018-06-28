5b23774e0f4e131c272970dbf9e50ea098c55919
Tsubasa Namiki <namikingsoft@gmail.com>
2018-06-29 07:52:48 +0900

    feat: hook comment

--- index.js
+++ index.js
@@ -9,15 +9,9 @@ module.exports = app => {
   // For more information on building apps:
   // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-  app.on('issues.opened', async context => {
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = context.issue({body: 'Hello World!'})
-
-    // Post a comment on the issue
+  app.on('issue_comment.created', async context => {
+    app.log('Added comment')
+    const params = context.issue({ body: 'Hello World!' })
     return context.github.issues.createComment(params)
   })
 }
